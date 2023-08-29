7be6884d0b009101cc80060152bee9749b055487
solomonraza <solomonrazak99@gmsil.com>
2023-08-29 15:50:43 -0700

    more js problems solved

--- main.js
+++ main.js
@@ -53,3 +53,48 @@ prepand=' AM';
 
 console.log(`The time is ${hour}${prepand}: ${minutes} : ${seconds}`)
 
+
+
+
+// A JAVASCRIPT TO PRINT THE CURRENT WINDOW CONTENT.
+//function to print current window.
+function print_current_page(){
+    window.print();
+}
+
+
+
+
+// JAVASCRIPT PROGRAM TO GET THE CURRENT DATE.
+// Expected Output :
+// mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy
+
+let todayy = new Date(); // to get the current date and time.
+
+let dd = todayy.getDate() // to get the current date of the month from 1 - 31
+let mm = todayy.getMonth() // to get the month from 1 - 12
+let yyyy = todayy.getFullYear() // to get the year
+
+if (dd < 10){
+    dd = '0'+dd;
+}
+if (mm < 10){
+    mm = '0'+mm;
+}
+console.log(`The current date is ${mm}-${dd}-${yyyy}`);
+console.log(`The current date is ${mm}/${dd}/${yyyy}`);
+console.log(`The current date is ${dd}-${mm}-${yyyy}`);
+console.log(`The current date is ${dd}/${mm}/${yyyy}`);
+
+
+
+// A jAVASCRIPT PROGRAM TO FIND THE AREA OF A TRIANGLE.
+// OF LENGTHS 5 6 AND 7
+
+
+
+function areaOfTriangle(base, height, hypotenuse){
+    return 1/2 * base * height;
+} 
+
+console.log("the area of the triangle is " + areaOfTriangle(5,6,7));
